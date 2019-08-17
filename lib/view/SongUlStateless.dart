7e42fab62fd8f1fd4a9e96677575b756af417c3d
DangerBlack <danger.recheng@hotmail.it>
2019-08-17 20:14:02 +0200

    fix: Fixed wrong UX experience with long text

--- lib/view/SongUlStateless.dart
+++ lib/view/SongUlStateless.dart
@@ -12,16 +12,17 @@ import '../model/Constants.dart';
 class SongUlStateless extends StatelessWidget {
   final _biggerFont = const TextStyle(fontSize: 18.0);
   final User user;
-  final String title;// = "Elenco canzoni";
+  final String title; // = "Elenco canzoni";
   final SongList l = new SongList();
 
-  SongUlStateless(List<Song> songs,this.title, this.user):super(){
+  SongUlStateless(List<Song> songs, this.title, this.user) : super() {
     //this.title = title;
     //this.user = user;
     this.l.list = songs;
     //updateList(songs);
   }
-  updateList(List<Song> songs){
+
+  updateList(List<Song> songs) {
     this.l.list = songs;
   }
 
@@ -44,11 +45,13 @@ class SongUlStateless extends StatelessWidget {
         ],
       ),
       body: buildList(context),
-      floatingActionButton: user.logged?FloatingActionButton(
-        onPressed: () => _showConfirmDialog(context),
-        tooltip: AppLocalizations.of(context).add,
-        child: Icon(Icons.add),
-      ):null,
+      floatingActionButton: user.logged
+          ? FloatingActionButton(
+              onPressed: () => _showConfirmDialog(context),
+              tooltip: AppLocalizations.of(context).add,
+              child: Icon(Icons.add),
+            )
+          : null,
     );
   }
 
@@ -57,47 +60,55 @@ class SongUlStateless extends StatelessWidget {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-
           title: new Text(AppLocalizations.of(context).create_dialog_title),
           content: new Container(
-                height: 200,
-                child: new Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                      new Text(AppLocalizations.of(context).create_dialog_body),
-                      new Text(""),
-                      new Text(AppLocalizations.of(context).create_dialog_body_sample,
-                          style: TextStyle(fontStyle: FontStyle.italic),textAlign: TextAlign.left),
-                      new FlatButton(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            new Text(AppLocalizations.of(context).create_dialog_body_more + " ",
-                              style: TextStyle(fontSize: 17),
-                            ),
-                            Text("ChordPro",
-                              style: TextStyle(
-                                fontSize: 17,
-                                decoration: TextDecoration.underline,
-                              ),
-                            ),
-                          ],
+            height: 200,
+            width: 200,
+            child: new Center(
+              child: new ListView(
+                scrollDirection: Axis.vertical,
+                children: <Widget>[
+                  new Text(AppLocalizations.of(context).create_dialog_body),
+                  new Text(""),
+                  new Text(
+                      AppLocalizations.of(context).create_dialog_body_sample,
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                      textAlign: TextAlign.left),
+                  new FlatButton(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        new Text(
+                          AppLocalizations.of(context).create_dialog_body_more +
+                              " ",
+                          style: TextStyle(fontSize: 17),
                         ),
-                        onPressed: () => Utils.launchURL(Constants.urlChordPro),
-                      ),
-                  ],
+                        Text(
+                          "ChordPro",
+                          style: TextStyle(
+                            fontSize: 17,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ],
+                    ),
+                    onPressed: () => Utils.launchURL(Constants.urlChordPro),
+                  ),
+                ],
               ),
+            ),
           ),
           actions: <Widget>[
             new FlatButton(
-              child: new Text(AppLocalizations.of(context).dialog_confirm.toUpperCase()),
+              child: new Text(
+                  AppLocalizations.of(context).dialog_confirm.toUpperCase()),
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => CreateSongStatefull(title: AppLocalizations.of(context).create_song),
+                    builder: (context) => CreateSongStatefull(
+                        title: AppLocalizations.of(context).create_song),
                   ),
                 );
               },
@@ -108,25 +119,25 @@ class SongUlStateless extends StatelessWidget {
     );
   }
 
-  Widget buildSongRow(BuildContext context,Song pair, int index) {
+  Widget buildSongRow(BuildContext context, Song pair, int index) {
     return ListTile(
-        leading: const Icon(Icons.music_note),
-        title: Text(
-          pair.title,
-          style: _biggerFont,
-        ),
-        subtitle: Text(
-          pair.author ?? "",
-        ),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SongText(song: pair)),
-          );
-        },
-        onLongPress: (){
-          print("wow!");
-        },
+      leading: const Icon(Icons.music_note),
+      title: Text(
+        pair.title,
+        style: _biggerFont,
+      ),
+      subtitle: Text(
+        pair.author ?? "",
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SongText(song: pair)),
+        );
+      },
+      onLongPress: () {
+        print("wow!");
+      },
     );
   }
 
@@ -140,7 +151,7 @@ class SongUlStateless extends StatelessWidget {
           int index = i ~/ 2;
           if (index <= l.list.length) {
             var s = l.get(index);
-            return buildSongRow(context,s,index);
+            return buildSongRow(context, s, index);
           } else {
             //TODO: Statement unreachable!!!!
             return null;
